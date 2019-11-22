471619a972ad1db146bc0873908a4107c1281215
Philippe Laval <philippe.laval.1968@laposte.net>
2019-11-22 18:47:54 +0100

    Add some examples

---
+++ DotNetRDFExample/PlayWithGraph.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using VDS.RDF.Parsing.Handlers;
+using VDS.RDF.Writing;
+
+namespace PlayWithDotNetRDF
+{
+    public class PlayWithGraph
+    {
+        /// <summary>
+        /// https://github.com/dotnetrdf/dotnetrdf/wiki/UserGuide-Library-Overview
+        /// </summary>
+        public void PlayWithOverview()
+        {
+            /// Get a new Graph and set it's Base URI
+            IGraph g = new Graph();
+            g.BaseUri = new Uri("http://example.org/");
+
+            #region URI Nodes
+
+            // Create a URI Node that refers to the Base URI of the Graph
+            // Only valid when the Graph has a non-null Base URI
+            IUriNode thisGraph = g.CreateUriNode();
+
+            // Create a URI Node that refers to some specific URI
+            IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
+
+            // Create a URI Node using a Prefixed Name
+            // Need to define a Namespace first
+            g.NamespaceMap.AddNamespace("ex", UriFactory.Create("http://example.org/namespace/"));
+            IUriNode pname = g.CreateUriNode("ex:demo");
+            // Resulting URI is http://example.org/namespace/demo
+
+            #endregion
+
+            #region Blank Nodes
+
+            //Create an anonymous Blank Node
+            //Each call to this method generates a Blank Node with a new unique identifier within the Graph
+            IBlankNode anon = g.CreateBlankNode();
+
+            Console.WriteLine(anon.InternalID);
+
+            //Create a named Blank Node
+            //Reusing the same ID results in the same Blank Node within the Graph
+            //Note that if the ID refers to an automatically assigned ID that is already in use the returned
+            //Blank Node will be given an alternative ID
+            IBlankNode named = g.CreateBlankNode("ID");
+
+            Console.WriteLine(named.InternalID);
+
+            #endregion
+
+            #region Literal Nodes
+
+            // Create a Plain Literal
+            ILiteralNode plain = g.CreateLiteralNode("some value");
+
+            //Create some Language Specified Literal
+            ILiteralNode hello = g.CreateLiteralNode("hello", "en");
+            ILiteralNode bonjour = g.CreateLiteralNode("bonjour", "fr");
+
+            //Create some typed Literals
+            //You'll need to be using the VDS.RDF.Parsing namespace to reference the constants used here
+            ILiteralNode oneLiteral = g.CreateLiteralNode("1", UriFactory.Create(XmlSpecsHelper.XmlSchemaDataTypeInteger));
+            ILiteralNode trueLiteral = g.CreateLiteralNode("true", UriFactory.Create(XmlSpecsHelper.XmlSchemaDataTypeBoolean));
+
+            #endregion
+
+            #region Triples
+
+            //Create some Nodes
+            //IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
+            IUriNode createdBy = g.CreateUriNode(UriFactory.Create("http://example.org/createdBy"));
+            ILiteralNode robVesse = g.CreateLiteralNode("Rob Vesse");
+
+            // Assert this Triple
+            Triple t1 = new Triple(dotNetRDF, createdBy, robVesse);
+            g.Assert(t1);
+
+            // Retract
+            // g.Retract(t1);
+
+            #endregion
+
+
+            //Loop through Triples
+            foreach (Triple t in g.Triples)
+            {
+                Console.WriteLine(t.ToString());
+            }
+
+
+
+        }
+
+        /// <summary>
+        /// https://github.com/dotnetrdf/dotnetrdf/wiki/UserGuide-Hello-World
+        /// </summary>
+        public void PlayWithHelloWorld()
+        {
+            IGraph g = CreateGraph();
+
+            // Console.ReadLine();
+
+            NTriplesWriter ntwriter = new NTriplesWriter();
+            ntwriter.Save(g, "HelloWorld.nt");
+
+            RdfXmlWriter rdfxmlwriter = new RdfXmlWriter();
+            rdfxmlwriter.Save(g, "HelloWorld.rdf");
+
+            CompressingTurtleWriter turtleWriter = new CompressingTurtleWriter();
+            turtleWriter.Save(g, "HelloWorld.ttl");
+        }
+
+        private static IGraph CreateGraph()
+        {
+            IGraph g = new Graph();
+            g.BaseUri = new Uri("http://example.org/");
+
+            IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
+            IUriNode says = g.CreateUriNode(UriFactory.Create("http://example.org/says"));
+            ILiteralNode helloWorld = g.CreateLiteralNode("Hello World");
+            ILiteralNode bonjourMonde = g.CreateLiteralNode("Bonjour tout le Monde", "fr");
+
+            g.Assert(new Triple(dotNetRDF, says, helloWorld));
+            g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
+
+            foreach (Triple t in g.Triples)
+            {
+                Console.WriteLine(t.ToString());
+            }
+
+            return g;
+        }
+
+        /// <summary>
+        /// https://github.com/dotnetrdf/dotnetrdf/wiki/UserGuide-Reading-RDF
+        /// </summary>
+        public void PlayWithReadingRdf()
+        {
+            #region Reading RDF from Files
+            IGraph g = new Graph();
+            IGraph h = new Graph();
+            TurtleParser ttlparser = new TurtleParser();
+
+            // Load using a Filename
+            ttlparser.Load(g, "HelloWorld.ttl");
+
+            // Load using a StreamReader
+            ttlparser.Load(h, new StreamReader("HelloWorld.ttl"));
+
+            try
+            {
+                IGraph g2 = new Graph();
+                NTriplesParser ntparser = new NTriplesParser();
+
+                //Load using Filename
+                ntparser.Load(g2, "HelloWorld.nt");
+            }
+            catch (RdfParseException parseEx)
+            {
+                //This indicates a parser error e.g unexpected character, premature end of input, invalid syntax etc.
+                Console.WriteLine("Parser Error");
+                Console.WriteLine(parseEx.Message);
+            }
+            catch (RdfException rdfEx)
+            {
+                //This represents a RDF error e.g. illegal triple for the given syntax, undefined namespace
+                Console.WriteLine("RDF Error");
+                Console.WriteLine(rdfEx.Message);
+            }
+            #endregion
+
+            #region Reading RDF from URIs
+
+            try
+            {
+                IGraph g3 = new Graph();
+                //UriLoader.Load(g3, new Uri("http://dbpedia.org/resource/Barack_Obama"));
+            }
+            catch (RdfParseException parseEx)
+            {
+                //This indicates a parser error e.g unexpected character, premature end of input, invalid syntax etc.
+                Console.WriteLine("Parser Error");
+                Console.WriteLine(parseEx.Message);
+            }
+            catch (RdfException rdfEx)
+            {
+                //This represents a RDF error e.g. illegal triple for the given syntax, undefined namespace
+                Console.WriteLine("RDF Error");
+                Console.WriteLine(rdfEx.Message);
+            }
+            #endregion
+
+            #region Reading RDF from Embedded Resources
+
+            try
+            {
+                IGraph g4 = new Graph();
+                EmbeddedResourceLoader.Load(g4, "embedded.ttl, PlayWithDotNetRDF");
+                Console.WriteLine(g4.IsEmpty);
+            }
+            catch (RdfParseException parseEx)
+            {
+                //This indicates a parser error e.g unexpected character, premature end of input, invalid syntax etc.
+                Console.WriteLine("Parser Error");
+                Console.WriteLine(parseEx.Message);
+            }
+            catch (RdfException rdfEx)
+            {
+                //This represents a RDF error e.g. illegal triple for the given syntax, undefined namespace
+                Console.WriteLine("RDF Error");
+                Console.WriteLine(rdfEx.Message);
+            }
+            #endregion
+
+            #region Reading RDF from Strings
+
+            Graph g5 = new Graph();
+            StringParser.Parse(g5, "<http://example.org/a> <http://example.org/b> <http://example.org/c>.");
+
+            Graph g6 = new Graph();
+            NTriplesParser parser = new NTriplesParser();
+            parser.Load(g6, new StringReader("<http://example.org/a> <http://example.org/b> <http://example.org/c>."));
+
+            #endregion
+
+            #region Store Parsers
+
+            /*
+            TripleStore store = new TripleStore();
+            TriGParser trigparser = new TriGParser();
+
+            //Load the Store
+            trigparser.Load(store, "Example.trig");
+            */
+
+            #endregion
+
+            #region Advanced Parsing
+
+            // Create a Handler and use it for parsing
+            CountHandler handler = new CountHandler();
+            TurtleParser turtleParser = new TurtleParser();
+            turtleParser.Load(handler, "HelloWorld.ttl");
+
+            //Print the resulting count
+            Console.WriteLine(handler.Count + " Triple(s)");
+
+            // https://github.com/dotnetrdf/dotnetrdf/wiki/UserGuide-Handlers-API
+            #endregion
+
+            /*
+Parser Class 	    Supported Input
+NTriplesParser 	    NTriples
+Notation3Parser 	Notation 3, Turtle, NTriples, some forms of TriG
+NQuadsParser 	    NQuads, NTriples
+RdfAParser 	        RDFa 1.0 embedded in (X)HTML, some RDFa 1.1 support
+RdfJsonParser 	    RDF/JSON (Talis specification)
+RdfXmlParser 	    RDF/XML
+TriGParser 	        TriG
+TriXParser 	        TriX
+TurtleParser 	    Turtle, NTriples
+JsonLdParser 	    JSON-LD             
+             */
+
+        }
+
+        /// <summary>
+        /// https://github.com/dotnetrdf/dotnetrdf/wiki/UserGuide-Writing-RDF
+        /// </summary>
+        public void TesPlayWithWritingRdft()
+        {
+            #region Basic Usage
+
+            IGraph g = CreateGraph();
+
+            //Assume that the Graph to be saved has already been loaded into a variable g
+            RdfXmlWriter rdfxmlwriter = new RdfXmlWriter();
+
+            //Save to a File
+            rdfxmlwriter.Save(g, "Example.rdf");
+
+            //Save to a Stream
+            rdfxmlwriter.Save(g, Console.Out);
+
+            #region Writing to Strings
+
+            //Assume that the Graph to be saved has already been loaded into a variable g
+            //RdfXmlWriter rdfxmlwriter = new RdfXmlWriter();
+
+            String data1 = VDS.RDF.Writing.StringWriter.Write(g, rdfxmlwriter);
+
+            // or
+
+            //Assume that the Graph to be saved has already been loaded into a variable g
+            //RdfXmlWriter rdfxmlwriter = new RdfXmlWriter();
+            System.IO.StringWriter sw = new System.IO.StringWriter();
+
+            //Call the Save() method to write to the StringWriter
+            rdfxmlwriter.Save(g, sw);
+
+            //We can now retrieve the written RDF by using the ToString() method of the StringWriter
+            String data2 = sw.ToString();
+
+            #endregion
+
+
+            #endregion
+        }
+
+    }
+}
--- DotNetRDFExample/Program.cs
+++ DotNetRDFExample/Program.cs
@@ -1,12 +1,97 @@
 ﻿using System;
+using System.IO;
+using VDS.RDF;
+using VDS.RDF.Parsing;
 
-namespace DotNetRDFExample
+using VDS.RDF.Query;
+using VDS.RDF.Writing.Formatting;
+
+namespace PlayWithDotNetRDF
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+                PlayWithGraph p = new PlayWithGraph();
+                p.PlayWithOverview();
+                p.PlayWithHelloWorld();
+                p.PlayWithReadingRdf();
+                p.TesPlayWithWritingRdft();
+
+
+                Test1();
+
+                IGraph g = new Graph();
+
+                TurtleParser ttlparser = new TurtleParser();
+
+                //Load using a Filename
+                ttlparser.Load(g, @"C:\Users\philippel\source\repos\PlayWithDotNetRDF\PlayWithDotNetRDF\Files\Test12.ttl");
+
+                //First we need an instance of the SparqlQueryParser
+                SparqlQueryParser parser = new SparqlQueryParser();
+
+                //Then we can parse a SPARQL string into a query
+                string query = File.ReadAllText(@"C:\Users\philippel\source\repos\PlayWithDotNetRDF\PlayWithDotNetRDF\Files\query1.txt");
+
+                SparqlQuery q = parser.ParseFromString(query);
+
+                SparqlResultSet o = g.ExecuteQuery(q) as SparqlResultSet;
+
+                //Create a formatter
+                INodeFormatter formatter = new TurtleFormatter();
+                
+                foreach (SparqlResult result in o)
+                {
+                    Console.WriteLine(result.ToString(formatter));
+                }
+
+            }
+            catch (RdfParseException parseEx)
+            {
+                //This indicates a parser error e.g unexpected character, premature end of input, invalid syntax etc.
+                Console.WriteLine("Parser Error");
+                Console.WriteLine(parseEx.Message);
+            }
+            catch (RdfException rdfEx)
+            {
+                //This represents a RDF error e.g. illegal triple for the given syntax, undefined namespace
+                Console.WriteLine("RDF Error");
+                Console.WriteLine(rdfEx.Message);
+            }
+
+
         }
+
+        public static void Test1()
+        {
+            //Create a Parameterized String
+            SparqlParameterizedString queryString = new SparqlParameterizedString();
+
+            //Add a namespace declaration
+            queryString.Namespaces.AddNamespace("ex", new Uri("http://example.org/ns#"));
+
+            //Set the SPARQL command
+            //For more complex queries we can do this in multiple lines by using += on the
+            //CommandText property
+            //Note we can use @name style parameters here
+            queryString.CommandText = "SELECT * WHERE { ?s ex:property @value }";
+
+            //Inject a Value for the parameter
+            queryString.SetUri("value", new Uri("http://example.org/value"));
+
+            //When we call ToString() we get the full command text with namespaces appended as PREFIX
+            //declarations and any parameters replaced with their declared values
+            Console.WriteLine(queryString.ToString());
+
+            //We can turn this into a query by parsing it as in our previous example
+            SparqlQueryParser parser = new SparqlQueryParser();
+            SparqlQuery query = parser.ParseFromString(queryString);
+        }
+
+
     }
 }
