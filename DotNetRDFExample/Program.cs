a1bd2687ea937010068c809efa9808d6eb68cd5e
Philippe Laval <philippe.laval.1968@laposte.net>
2019-11-22 18:26:17 +0100

    Create the project for VS2017

---
+++ DotNetRDFExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetRDFExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
